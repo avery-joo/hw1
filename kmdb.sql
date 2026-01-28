ebec9d13f9d2046a657181567cb87021715475f2
avery-joo <sohyeong.joo@kellogg.northwestern.edu>
2026-01-28 14:06:35 +0000

    ERD and Drop Table

--- kmdb.sql
+++ kmdb.sql
@@ -95,6 +95,10 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS agents;
 
 -- Create new tables, according to your domain model
 -- TODO!
